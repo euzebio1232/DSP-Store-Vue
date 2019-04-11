69d68d25ae337035c2e35ab23508593bcc258777
wellyson dannilo <wellyson.dannilo@dce.ufpb.br>
2019-04-11 08:18:56 -0300

    Cadastro de Produtos finalizado

---
+++ dsp-store/src/components/cadastroProdutos.vue
@@ -0,0 +1,83 @@
+<template id="cadastroProdutos">
+    <div>
+        <form @submit="checkform">
+
+            <label>Categoria</label>
+            <input v-model="categoria" type="text" > <br/>
+
+            <label>Marca</label>
+            <input v-model="marca" type="text"><br/>
+
+            <label>nome</label>
+            <input v-model="nome" type="text"><br/>
+
+            <label>Tamanho</label>
+            <input v-model="tamanho" type="text"><br/>
+
+            <label>Valor</label>
+            <input v-model="valor" type="text"><br/>
+
+            <label>Descrição</label>
+            <input v-model="descricao" type="text"><br/>
+
+            <button>Cadastrar</button>
+
+        </form>
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+/* eslint-disable */
+export default {
+    template: "#cadastroProdutos",
+    data() {
+       return {
+        categoria: "",
+        marca: "",
+        nome: "",
+        tamanho: "",
+        valor: "",
+        descricao: "",
+    }},
+
+
+    methods:{
+        checkform: function (e) {
+
+            let headers = {
+                "x-access-token":window.localStorage.getItem("token")
+            }
+
+            let dados = {
+                    categoria: this.categoria,
+                    marca: this.marca,
+                    nome: this.nome,
+                    tamanho: this.tamanho,
+                    valor: this.valor,
+                    descricao: this.descricao,
+                    
+            }
+            
+            let payload = {
+                headers,
+                dados
+            }
+
+            axios
+            .post("http://localhost:3000/produtos", payload)
+            .then((response)=>{
+                console.log(response)
+            })
+
+            e.preventDefault();
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ dsp-store/src/components/produtos.vue
@@ -0,0 +1,62 @@
+<template id="produtos">
+    <ul>
+        <li v-for = "produto in produtos" :key="produto.id">
+            {{produto.nome}} - {{produto.valor}}
+        </li>
+    </ul>
+</template>
+
+<script>
+/* eslint-disable */
+import axios from 'axios'
+
+export default {
+    template: "#produtos",
+    data: function() {
+        return {
+            produtos: []
+        }
+    },
+
+    created: function() {
+        let cat = this.$route.params.categoria;
+        let url = "http://localhost:3000/produtos/"
+        if(cat !== undefined){
+            url+=cat;
+        }
+        
+
+        axios.get(url)
+            .then(response => {
+                console.log(response.data)
+                this.produtos = response.data
+            })
+            .catch(e => {
+                console.log("erro")
+            })
+    },
+
+    updated: function(l, a) {
+        let cat = this.$route.params.categoria;
+        let url = "http://localhost:3000/produtos/"
+        if(cat !== undefined){
+            url+=cat;
+        }
+        
+
+        axios.get(url)
+            .then(response => {
+                console.log(response.data)
+                this.produtos = response.data
+            })
+            .catch(e => {
+                console.log("erro")
+            })
+    }
+
+}
+</script>
+
+<style>
+
+</style>
